c500ff9cb826d48354502820d34664934b4f478c
mokranimoncef <moncef.mokrani@etu.univ-batna2.dz>
2024-10-19 22:55:14 +0100

    getting started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,33 @@
 import '@/styles/globals.css'
+import {Inter} from 'next/font/google'
+import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
+const inter = Inter({subsets : ["latin"]})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+      )}>
+      <body className='min-h-screen pt-12 antialiased bg-slate-50'>
+        <Navbar/>
+
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+        
+      </body>
     </html>
   )
 }
