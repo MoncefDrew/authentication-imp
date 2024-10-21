204064adefa20acb186a3e52a56bea834a572199
mokranimoncef <moncef.mokrani@etu.univ-batna2.dz>
2024-10-21 23:17:31 +0100

    gs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
+import  Link  from "next/Link";
 
 
 const Navbar = () =>{
     return(
-        <div>
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b borderzinc-300 z-[10] py-2">
+            <div className="container max-w-7x1 h-full mx-auto flex items-center justify-between gap-2">
+                {/* logo*/}
+                <Link href='/' className="flex gap-2 items-center">
+                    <p className="flex gap-2 items-center">Breaddit</p>
+
+                </Link>
+
+            </div>
         </div>
     )
 }
 export default Navbar;
-
-
- 
